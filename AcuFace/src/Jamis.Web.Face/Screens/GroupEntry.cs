e69ddb2bdc2e59f4da72cff1bb19daa9ca595388
svoytenko <svoytenko@jamis.com>
2018-01-27 17:53:03 -0500

    Added basic implementation to fetch the person groups

--- AcuFace/src/Jamis.Web.Face/Screens/GroupEntry.cs
+++ AcuFace/src/Jamis.Web.Face/Screens/GroupEntry.cs
@@ -1,8 +1,44 @@
 ﻿using PX.Data;
+using System.Collections;
+using System.Linq;
 
 namespace Jamis.Web.Face.Screens
 {
     public class GroupEntry : PXGraph<GroupEntry, PersonGroup>
     {
+        [PXVirtualDAC]
+        public PXSelect<PersonGroup> Groups;
+
+        [PXVirtualDAC]
+        public PXSelect<Person> Persons;
+
+        private IFaceApi Api;
+
+        public GroupEntry()
+        {
+            this.Api = this.GetFaceApi();
+        }
+
+        public IEnumerable groups()
+        {
+            return Api.GetGroups();
+        }
+
+        public IEnumerable persons()
+        {
+            var group = Groups.Current;
+
+            if (string.IsNullOrEmpty(group?.Name))
+            {
+                return Enumerable.Empty<Person>();
+            }
+
+            if (Groups.Cache.GetStatus(group) == PXEntryStatus.Inserted)
+            {
+                return Enumerable.Empty<Person>();
+            }
+
+            return Api.GetPersons(group.Name);
+        }
     }
 }
