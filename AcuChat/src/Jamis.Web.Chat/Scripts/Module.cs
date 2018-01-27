95b4a13064783ea4e3979157f470cec3cd0e15f3
Viktor Tratsevskyy <horster@gmail.com>
2018-01-27 18:43:15 -0500

    Register ChatHub with autofac service.

---
+++ AcuChat/src/Jamis.Web.Chat/Scripts/Module.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Jamis.Web.Chat.Scripts
+{
+    public static class Module
+    {
+        static public readonly Type BaseKey = typeof(Module);
+
+        private const string path = "Jamis.Web.Chat.Scripts.";
+
+        public const string WebType = "application/x-javascript";
+
+        public const string Key = "Jamis.Web.Chat";
+
+        public const string Script = path + "module.js";
+
+        static public readonly string[] Files = new string[] { Script };
+    }
+
+}
