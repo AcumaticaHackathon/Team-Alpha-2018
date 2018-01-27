daf10c884a6a3a8e49469bd1b7adbd50652cd570
Viktor Tratsevskyy <horster@gmail.com>
2018-01-27 18:31:27 -0500

    Added SignalR library

--- AcuChat/src/Jamis.Web.Chat/ChatHub.cs
+++ AcuChat/src/Jamis.Web.Chat/ChatHub.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.SignalR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Jamis.Web.Chat
 {
-    public class Class1
+    public class ChatHub : Hub
     {
     }
 }
