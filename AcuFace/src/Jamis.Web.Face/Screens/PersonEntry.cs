99eee5b7f52726178ce359d2a507c32b383beabe
svoytenko <svoytenko@jamis.com>
2018-01-27 17:53:52 -0500

    Added basic implementation to fetch persons

--- AcuFace/src/Jamis.Web.Face/Screens/PersonEntry.cs
+++ AcuFace/src/Jamis.Web.Face/Screens/PersonEntry.cs
@@ -1,8 +1,29 @@
 ﻿using PX.Data;
+using System.Collections;
 
 namespace Jamis.Web.Face.Screens
 {
     public class PersonEntry : PXGraph<PersonEntry, Person>
     {
+        [PXVirtualDAC]
+        public PXSelect<Person> Persons;      
+
+        private IFaceApi Api;
+
+        public PersonEntry()
+        {
+            this.Api = this.GetFaceApi();
+        }
+
+        public IEnumerable persons()
+        {
+            foreach (var group in Api.GetGroups())
+            {
+                foreach (var person in Api.GetPersons(group.Name))
+                {
+                    yield return person;
+                }
+            }
+        }
     }
 }
